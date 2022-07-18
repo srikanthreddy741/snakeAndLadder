ffb2bbee40442a153421e9132ba7f78260c77a50
srikanthreddy741 <bandarisrikanth741@gmail.com>
2022-07-18 22:44:17 +0530

    added die value

---
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("WelCome To Snake And Ladder");
+
+int position = 0;
+Random random = new Random();
+int rolldie = random.Next(1, 7);
+
+Console.WriteLine("player 1 position is " + position);
+Console.WriteLine("Roll Die value; " + rolldie);
