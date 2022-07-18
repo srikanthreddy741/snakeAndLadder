292f2b7531414f2499b5a311890f2f31989c4dd8
srikanthreddy741 <bandarisrikanth741@gmail.com>
2022-07-18 22:14:49 +0530

    added position 0

---
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("welcome To Snake And Ladder");
+
+int position = 0;
+
+Console.WriteLine("player 1 position is " + position);
