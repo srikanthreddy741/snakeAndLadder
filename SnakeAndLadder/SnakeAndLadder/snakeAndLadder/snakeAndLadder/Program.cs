1164efe91449507f4677fec105e2b4725e262707
srikanthreddy741 <bandarisrikanth741@gmail.com>
2022-07-22 14:53:42 +0530

    added positon100

--- SnakeAndLadder/SnakeAndLadder/snakeAndLadder/snakeAndLadder/Program.cs
+++ SnakeAndLadder/SnakeAndLadder/snakeAndLadder/snakeAndLadder/Program.cs
@@ -1,28 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("WelCome To Snake And Ladder");
-
-Program SnakeAndLadder;
-
-int position = 0;
-const int LADDER = 1;
-const int SNAKE = 2;
-
-Random random = new Random();
-int option = random.Next(0,3);
-int rollDie = random.Next(1,7);
-switch (option)
+﻿namespace SnakeAndLadderGame
 {
-    case LADDER:
-        position = position + rollDie;
-        break;
-    case SNAKE :
-       position -=  rollDie;
-        break;
-    default:
-        
-        break;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Simulate Snake And Ladder Game");
+
+            GameStart.GameIsStart();
+        }
+    }
 }
-Console.WriteLine( position);
+
 
 
 
---
+++ SnakeAndLadder/SnakeAndLadder/snakeAndLadder/snakeAndLadder/position100.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderGame
+{
+    public class GameStart
+    {
+        const int NO_PLAY = 1;
+        const int LADDER = 2;
+        const int SNAKE = 3;
+        public static void GameIsStart()
+        {
+            int Player = 1;
+            int Player_Position = 0;
+
+            Console.WriteLine("Player Position is : " + Player_Position);
+
+            Random random = new Random();
+            while (Player_Position != 100)
+            {
+                int DieRoll = random.Next(1, 7);
+                Console.WriteLine("Rolling the Die : " + DieRoll);
+
+                int check = random.Next(1, 4);
+                switch (check)
+                {
+                    case NO_PLAY:
+                        Player_Position = Player_Position + 0;
+                        break;
+                    case LADDER:
+                        Player_Position = Player_Position + DieRoll;
+                        break;
+                    case SNAKE:
+                        Player_Position = Player_Position - DieRoll;
+                        if (Player_Position < 0)
+                            Player_Position = 0;
+                        break;
+                }
+                Console.WriteLine("After Rollint Die Palyer Position is : " + Player_Position);
+            }
+        }
+    }
+}
